d692de9dbb47267fc3f3f80d24d7f8f4f65b980e
Matthew Davey <matthew.davey@dotbunny.com>
2021-03-06 14:29:47 -0500

    Add license notices

--- GDX/Runtime/Collections/Pooling/IManagedPool.cs
+++ GDX/Runtime/Collections/Pooling/IManagedPool.cs
@@ -1,3 +1,6 @@
+// dotBunny licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace GDX.Collections.Pooling
 {
     /// <summary>
