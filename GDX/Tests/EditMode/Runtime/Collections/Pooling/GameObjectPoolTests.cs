ffb07817cd4fb68e9095bfb411e2cb3065f88210
Matthew Davey <matthew.davey@dotbunny.com>
2021-03-12 20:32:33 -0500

    Stub tests for interface

--- GDX/Tests/EditMode/Runtime/Collections/Pooling/GameObjectPoolTests.cs
+++ GDX/Tests/EditMode/Runtime/Collections/Pooling/GameObjectPoolTests.cs
@@ -9,14 +9,66 @@ namespace Runtime.Collections.Pooling
 {
     public class GameObjectPoolTests
     {
+        private class GameObjectPoolItemTest : MonoBehaviour, IGameObjectPoolItem
+        {
+            /// <inheritdoc />
+            public GameObject GetGameObject()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public IManagedPool GetParentPool()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public bool IsValidItem()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void OnReturnedToPool()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void OnSpawnedFromPool()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void ReturnToPool()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+            public void SetParentPool(IManagedPool targetManagedPool)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
         private GameObject _mockBaseObject;
+        private GameObject _mockBaseObjectWithInterface;
         private Transform _mockTransform;
+        private Transform _mockBaseObjectWithInterfaceTransform;
 
         [SetUp]
         public void Setup()
         {
             _mockBaseObject = new GameObject();
             _mockTransform = new GameObject().transform;
+
+
+            _mockBaseObjectWithInterface = new GameObject();
+            _mockBaseObjectWithInterface.AddComponent<GameObjectPoolItemTest>();
+            _mockBaseObjectWithInterfaceTransform = new GameObject().transform;
         }
 
         [TearDown]
@@ -24,6 +76,27 @@ namespace Runtime.Collections.Pooling
         {
             Object.DestroyImmediate(_mockBaseObject);
             Object.DestroyImmediate(_mockTransform.gameObject);
+
+            Object.DestroyImmediate(_mockBaseObjectWithInterface);
+            Object.DestroyImmediate(_mockBaseObjectWithInterfaceTransform.gameObject);
+        }
+
+        [Test]
+        [Category("GDX.Tests")]
+        public void Get_MockData_DontTrigger()
+        {
+            IManagedPool pool = GameObjectPool.GetOrCreatePool(_mockBaseObjectWithInterface, _mockBaseObjectWithInterfaceTransform);
+
+            pool.TearDown();
+        }
+
+        [Test]
+        [Category("GDX.Tests")]
+        public void Get_MockData_Trigger()
+        {
+            IManagedPool pool = GameObjectPool.GetOrCreatePool(_mockBaseObjectWithInterface, _mockBaseObjectWithInterfaceTransform);
+
+            pool.TearDown();
         }
 
         [Test]
