675615e5c663414e52f448a1a982a77dd646f480
adamstrick <strick.adam@gmail.com>
2021-03-18 22:04:18 -0500

    Moved array pools and changed namespace.

--- GDX/Runtime/Collections/Pooling/ManagedArrayPool.cs
+++ GDX/Runtime/Collections/Pooling/ManagedArrayPool.cs
@@ -1,7 +1,7 @@
 // dotBunny licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace GDX.Collections
+namespace GDX.Collections.Pooling
 {
     public struct ManagedArrayPool<T>
     {
