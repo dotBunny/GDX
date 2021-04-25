c152815416a08716af9e9e4b52041723ca0e02f5
Matthew Davey <matthew.davey@dotbunny.com>
2021-04-25 13:36:03 -0400

    Include copyright notice thingy

--- GDX/Runtime/Trace.cs
+++ GDX/Runtime/Trace.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2020-2021 dotBunny Inc.
+// Copyright (c) 2020-2021 dotBunny Inc.
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
 
