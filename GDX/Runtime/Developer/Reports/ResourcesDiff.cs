e3683e0dbe25c15496e9871fa7822a389ef2ff23
Matthew Davey <matthew.davey@dotbunny.com>
2021-05-05 23:24:31 -0400

    Build out section

--- GDX/Runtime/Developer/Reports/ResourcesDiff.cs
+++ GDX/Runtime/Developer/Reports/ResourcesDiff.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using GDX.Collections.Generic;
 using GDX.Developer.Reports.Elements;
 using GDX.Developer.Reports.Objects;
+using GDX.Developer.Reports.Sections;
 
 namespace GDX.Developer.Reports
 {
@@ -21,32 +22,17 @@ namespace GDX.Developer.Reports
         public readonly Dictionary<Type, LongDiff> KnownUsage =
             new SerializableDictionary<Type, LongDiff>();
 
-        public readonly LongDiff MonoHeapSize;
-        public readonly LongDiff MonoUsedSize;
+        public readonly MemoryDiffSection MemoryContext;
+
         public readonly IntegerDiff ObjectCount;
 
         public readonly Dictionary<Type, Dictionary<TransientReference, ObjectInfo>> RemovedObjects =
             new Dictionary<Type, Dictionary<TransientReference, ObjectInfo>>();
 
-        public readonly LongDiff UnityGraphicsDriverAllocated;
-        public readonly LongDiff UnityTotalAllocatedMemory;
-        public readonly LongDiff UnityTotalReservedMemory;
-        public readonly LongDiff UnityTotalUnusedReservedMemory;
-
         public ResourcesDiff(ResourcesAudit lhs, ResourcesAudit rhs)
         {
-            // Simple difference values
             ObjectCount = new IntegerDiff(lhs.ObjectCount, rhs.ObjectCount);
-            MonoUsedSize = new LongDiff(lhs.MemoryContext.MonoUsedSize, rhs.MemoryContext.MonoUsedSize);
-            MonoHeapSize = new LongDiff(lhs.MemoryContext.MonoHeapSize, rhs.MemoryContext.MonoHeapSize);
-            UnityTotalAllocatedMemory =
-                new LongDiff(lhs.MemoryContext.UnityTotalAllocatedMemory, rhs.MemoryContext.UnityTotalAllocatedMemory);
-            UnityTotalReservedMemory =
-                new LongDiff(lhs.MemoryContext.UnityTotalReservedMemory, rhs.MemoryContext.UnityTotalReservedMemory);
-            UnityTotalUnusedReservedMemory = new LongDiff(lhs.MemoryContext.UnityTotalUnusedReservedMemory,
-                lhs.MemoryContext.UnityTotalUnusedReservedMemory);
-            UnityGraphicsDriverAllocated = new LongDiff(lhs.MemoryContext.UnityGraphicsDriverAllocatedMemory,
-                rhs.MemoryContext.UnityGraphicsDriverAllocatedMemory);
+            MemoryContext = MemoryDiffSection.Get(lhs.MemoryContext, rhs.MemoryContext);
 
             // Known Usage Changes
             foreach (KeyValuePair<Type, long> kvp in lhs.KnownUsage)
@@ -56,6 +42,7 @@ namespace GDX.Developer.Reports
                         ? new LongDiff(kvp.Value, rhs.KnownUsage[kvp.Key])
                         : new LongDiff(kvp.Value, 0));
             }
+
             foreach (KeyValuePair<Type, long> kvp in rhs.KnownUsage)
             {
                 if (!lhs.KnownUsage.ContainsKey(kvp.Key))
---
+++ GDX/Runtime/Developer/Reports/Sections/MemoryDiffSection.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2020-2021 dotBunny Inc.
+// dotBunny licenses this file to you under the BSL-1.0 license.
+// See the LICENSE file in the project root for more information.
+
+using GDX.Developer.Reports.Elements;
+
+namespace GDX.Developer.Reports.Sections
+{
+    public readonly struct MemoryDiffSection
+    {
+        public readonly LongDiff MonoHeapSize;
+        public readonly LongDiff MonoUsedSize;
+        public readonly LongDiff UnityGraphicsDriverAllocated;
+        public readonly LongDiff UnityTotalAllocatedMemory;
+        public readonly LongDiff UnityTotalReservedMemory;
+        public readonly LongDiff UnityTotalUnusedReservedMemory;
+
+        public MemoryDiffSection(MemorySection lhs, MemorySection rhs)
+        {
+            MonoUsedSize = new LongDiff(lhs.MonoUsedSize, rhs.MonoUsedSize);
+            MonoHeapSize = new LongDiff(lhs.MonoHeapSize, rhs.MonoHeapSize);
+            UnityTotalAllocatedMemory =
+                new LongDiff(lhs.UnityTotalAllocatedMemory, rhs.UnityTotalAllocatedMemory);
+            UnityTotalReservedMemory =
+                new LongDiff(lhs.UnityTotalReservedMemory, rhs.UnityTotalReservedMemory);
+            UnityTotalUnusedReservedMemory = new LongDiff(lhs.UnityTotalUnusedReservedMemory,
+                lhs.UnityTotalUnusedReservedMemory);
+            UnityGraphicsDriverAllocated = new LongDiff(lhs.UnityGraphicsDriverAllocatedMemory,
+                rhs.UnityGraphicsDriverAllocatedMemory);
+        }
+
+        public static MemoryDiffSection Get(MemorySection lhs, MemorySection rhs)
+        {
+            return new MemoryDiffSection(lhs, rhs);
+        }
+
+    }
+}
