8b44dac0eb3d3d8f7be7c53fb16a04a17f262524
Matthew Davey <matthew.davey@dotbunny.com>
2021-05-15 20:58:50 -0400

    Remove int scale for now, as really no.

--- GDX/Runtime/Array2DExtensions.cs
+++ GDX/Runtime/Array2DExtensions.cs
@@ -58,48 +58,5 @@ namespace GDX
 
             return returnArray;
         }
-
-        /// <summary>
-        ///     Generate an array scaled by bilinear interpolation.
-        /// </summary>
-        /// <remarks>Works with <see cref="int"/> values.</remarks>
-        /// <param name="inputArray">The existing <see cref="Array2D{T}"/> to scale.</param>
-        /// <param name="scaleFactor">The multiple to scale by.</param>
-        public static Array2D<int> Scale(this Array2D<int> inputArray, int scaleFactor = 2)
-        {
-            int originalRowsMinusOne = inputArray.RowCount - 1;
-            int originalColsMinusOne = inputArray.ColumnCount - 1;
-
-            int newRows = inputArray.RowCount * scaleFactor;
-            int newCols = inputArray.ColumnCount * scaleFactor;
-
-            Array2D<int> returnArray = new Array2D<int>(inputArray.RowCount * scaleFactor,
-                inputArray.ColumnCount * scaleFactor);
-
-            for (int x = 0; x < newCols; x++)
-            for (int y = 0; y < newRows; y++)
-            {
-                int gx = x / newCols * originalColsMinusOne;
-                int gy = y / newCols * originalRowsMinusOne;
-                int gxi = gx;
-                int gyi = gy;
-
-                int c00 = inputArray[gxi, gyi];
-                int c10 = inputArray[gxi + 1, gyi];
-                int c01 = inputArray[gxi, gyi + 1];
-                int c11 = inputArray[gxi + 1, gyi + 1];
-
-                int tx = gx - gxi;
-                int ty = gy - gyi;
-
-#if GDX_MATHEMATICS
-                returnArray[x, y] = (int)math.lerp(math.lerp(c00, c10, tx), math.lerp(c01, c11, tx), ty);
-#else
-                returnArray[x, y] = (int)Mathf.Lerp(Mathf.Lerp(c00, c10, tx), Mathf.Lerp(c01, c11, tx), ty);
-#endif
-            }
-
-            return returnArray;
-        }
     }
 }
