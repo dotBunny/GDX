daf36c8ff4ede369e6a5d09864d2f3c9343eb3d4
Matthew Davey <matthew.davey@dotbunny.com>
2021-11-06 17:15:51 -0400

    Stage some initial framing for DOTS runtime

--- Runtime/Collections/Pooling/GameObjectPool.cs
+++ Runtime/Collections/Pooling/GameObjectPool.cs
@@ -2,6 +2,8 @@
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
 
+
+#if !DOTS_RUNTIME
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -503,3 +505,5 @@ namespace GDX.Collections.Pooling
         }
     }
 }
+
+#endif
