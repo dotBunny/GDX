464ebca2438586668d82e2c2347dbccf640b14b4
Matthew Davey <matthew.davey@dotbunny.com>
2021-11-10 12:30:24 -0500

    Add if def for entities

---
+++ Tests/EditMode/Editor/AutomationTests.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) 2020-2021 dotBunny Inc.
+// dotBunny licenses this file to you under the BSL-1.0 license.
+// See the LICENSE file in the project root for more information.
+
+using GDX.Editor;
+using NUnit.Framework;
+using UnityEditor;
+
+namespace Editor
+{
+    public class AutomationTests
+    {
+        /*
+         * CaptureCamera
+         * CaptureCameraToPNG
+         * CaptureCamera
+         * CaptureCameraToPNG
+         */
+
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void CaptureEditorWindow_SceneView_ReturnsTexture()
+        // {
+        //
+        // }
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void CaptureEditorWindowToPNG_SceneView_OutputsImage()
+        // {
+        //
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void CaptureFocusedEditorWindow_ReturnsTexture()
+        // {
+        //
+        // }
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void CaptureFocusedEditorWindowToPNG_OutputsImage()
+        // {
+        //
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void CaptureGameViewWindow_ReturnsTexture()
+        // {
+        //
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void CaptureGameViewWindowToPNG_OutputsImage()
+        // {
+        //
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void GenerateProjectFiles_CreatesSolution()
+        // {
+        //     Automation.GenerateProjectFiles();
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void GenerateProjectFiles_CreatesProjectFiles()
+        // {
+        //     Automation.GenerateProjectFiles();
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void GetWindow_SceneView_NotNull()
+        // {
+        //     EditorWindow sceneView = Automation.GetWindow<SceneView>();
+        // }
+        //
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void ResetEditor_ResetsLayout()
+        // {
+        //     //ProcessArguments_MockData_ContainsFlag()
+        // }
+    }
+}
---
+++ Tests/EditMode/Runtime/Jobs/ParallelFor/ColorCompareJobTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2020-2021 dotBunny Inc.
+// dotBunny licenses this file to you under the BSL-1.0 license.
+// See the LICENSE file in the project root for more information.
+
+using NUnit.Framework;
+using Unity.Collections;
+
+namespace Runtime.Jobs.ParallelFor
+{
+    public class ColorCompareJobTests
+    {
+        // [Test]
+        // [Category("GDX.Tests")]
+        // public void Compare_FiftyFifty_()
+        // {
+        //     //ProcessArguments_MockData_ContainsFlag()
+        // }
+        //
+        // var screenshot = TestFramework.TakeScreenshot(Vector3.zero, Quaternion.identity);
+        // var screenshotData = screenshot.GetPixelData<Color>(0);
+        // var arrayLength = screenshotData.Length;
+        // var percentages = new NativeArray<float>(arrayLength, Allocator.TempJob);
+        //
+        // var calcDifferencesJob = new ParallelColorCompareJob()
+        // {
+        //     A = screenshotData,
+        //     B = screenshotData,
+        //     Percentage = percentages
+        // };
+        //
+        // var handle = calcDifferencesJob.Schedule(arrayLength, 64);
+        // handle.Complete();
+        //
+        // float average = 0;
+        //     for (var i = 0; i < arrayLength; i++)
+        // {
+        //     average += percentages[i];
+        // }
+        // average = average / arrayLength;
+        //
+        // screenshotData.Dispose();
+        // percentages.Dispose();
+        //
+        // Assert.IsTrue(average == 1);
+    }
+}
