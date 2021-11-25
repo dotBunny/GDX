9ba89921bc7b25d47cf9d81b881e38ff656c4873
Matthew Davey <matthew.davey@dotbunny.com>
2021-11-24 22:11:05 -0500

    Restore initial gameview

--- Tests/EditMode/TestMonitor.cs
+++ Tests/EditMode/TestMonitor.cs
@@ -30,11 +30,11 @@ namespace GDX.Tests.EditMode
                 Editor.Automation.StashWindowLayout();
             }
 
-            // EditorWindow gameView = Editor.Automation.GetGameView();
-            // if (gameView != null)
-            // {
-            //     gameView.Show(true);
-            // }
+            EditorWindow gameView = Editor.Automation.GetGameView();
+            if (gameView != null)
+            {
+                gameView.Show(true);
+            }
 
             // Make sure our temp folder is absolutely clear at the start
             Editor.Automation.ClearTempFolder();
