d66c258837ca799b20d32a29fa329afec970d0a6
Matthew Davey <matthew.davey@dotbunny.com>
2022-02-06 09:24:21 -0500

    Add UIElements based override for DisableInspectorAttribute

--- GDX.Classic.Editor/PropertyDrawers/DisableInInspectorAttributePropertyDrawer.cs
+++ GDX.Classic.Editor/PropertyDrawers/DisableInInspectorAttributePropertyDrawer.cs
@@ -3,7 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace GDX.Classic.Editor.PropertyDrawers
 {
@@ -26,5 +28,14 @@ namespace GDX.Classic.Editor.PropertyDrawers
             GUI.enabled = true;
         }
 
+        /// <inheritdoc />
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            VisualElement container = new VisualElement();
+            PropertyField propertyField = new PropertyField(property);
+            container.Add(propertyField);
+            container.SetEnabled(false);
+            return container;
+        }
     }
 }
