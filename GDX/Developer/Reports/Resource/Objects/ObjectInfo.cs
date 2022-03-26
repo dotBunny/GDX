d837d1a1788ff062787211809128e4e7ef6bf8ba
Matthew Davey <matthew.davey@dotbunny.com>
2022-03-26 19:31:23 -0400

    Test 'is not' pattern matching support

--- GDX/Developer/Reports/Resource/Objects/ObjectInfo.cs
+++ GDX/Developer/Reports/Resource/Objects/ObjectInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2020-2022 dotBunny Inc.
+// Copyright (c) 2020-2022 dotBunny Inc.
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
 
@@ -108,7 +108,7 @@ namespace GDX.Developer.Reports.Resource.Objects
         /// <returns>1 if larger, otherwise 0.</returns>
         public int CompareTo(object obj)
         {
-            if (!(obj is ObjectInfo info))
+            if (obj is not ObjectInfo info)
             {
                 return 0;
             }
