8db72dc2faafe70daa45e04deb3068366dfa75d2
Matthew Davey <matthew.davey@dotbunny.com>
2022-03-30 21:36:00 -0400

    Remove not needed using

--- GDX/Collections/Pooling/SimpleListManagedPool.cs
+++ GDX/Collections/Pooling/SimpleListManagedPool.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using GDX.Collections.Generic;
 
 namespace GDX.Collections.Pooling
