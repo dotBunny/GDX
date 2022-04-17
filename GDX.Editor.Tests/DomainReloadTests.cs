6727bc73d206060a942d16734e5eb1944bf36bfd
Matthew Davey <matthew.davey@dotbunny.com>
2022-04-16 20:49:27 -0400

    Check for messages during domain reload / force compilation

---
+++ GDX.Editor.Tests/DomainReloadTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2020-2022 dotBunny Inc.
+// dotBunny licenses this file to you under the BSL-1.0 license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine.TestTools;
+using Application = UnityEngine.Application;
+
+namespace GDX.Editor
+{
+    /// <summary>
+    ///     A collection of unit tests to validate for issues when compiling or reloading the domain.
+    /// </summary>
+    public class DomainReloadTests
+    {
+        static string GetFilePath()
+        {
+            return Path.Combine(UnityEngine.Application.dataPath, "DomainReloadTest.cs");
+        }
+
+        static string GetRandomCode()
+        {
+            StringBuilder randomCode = new StringBuilder(10);
+
+            randomCode.Append(
+                Platform.SafeCharacterPool[Core.Random.NextInteger(0, Platform.CharacterPoolLengthExclusive)]);
+            randomCode.Append(
+                Platform.SafeCharacterPool[Core.Random.NextInteger(0, Platform.CharacterPoolLengthExclusive)]);
+            randomCode.Append(
+                Platform.SafeCharacterPool[Core.Random.NextInteger(0, Platform.CharacterPoolLengthExclusive)]);
+            randomCode.Append(
+                Platform.SafeCharacterPool[Core.Random.NextInteger(0, Platform.CharacterPoolLengthExclusive)]);
+            randomCode.Append(
+                Platform.SafeCharacterPool[Core.Random.NextInteger(0, Platform.CharacterPoolLengthExclusive)]);
+
+            return randomCode.ToString();
+        }
+
+        static string GetFileContent()
+        {
+            string randomCode = GetRandomCode();
+
+            Developer.TextGenerator code = new Developer.TextGenerator("    ", "{", "}");
+            code.AppendLine("// Generated domain reload test file.");
+            code.AppendLine("// ReSharper disable All");
+            code.AppendLine("#pragma warning disable");
+            code.AppendLine($"namespace DomainReloadTest_{randomCode}");
+            code.PushIndent();
+            code.AppendLine($"public class Test_{randomCode}");
+            code.PushIndent();
+            code.AppendLine("[UnityEngine.Scripting.Preserve]");
+            code.AppendLine($"public void Init()");
+            code.PushIndent();
+            code.AppendLine("UnityEngine.Debug.Log(\"Domain Reload Test\");");
+
+            return code.ToString();
+        }
+
+        [UnityTest]
+        [Category(Core.TestCategory)]
+        public IEnumerator Recompile_NoUnexpectedMessages()
+        {
+            File.WriteAllText(GetFilePath(),GetFileContent());
+            AssetDatabase.ImportAsset(GetFilePath().Replace(Application.dataPath + "\\", "Assets/"));
+            yield return new RecompileScripts();
+
+            // Any warning or error will fail this
+            LogAssert.NoUnexpectedReceived();
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            string filePath = GetFilePath();
+            Platform.ForceDeleteFile(filePath);
+            Platform.ForceDeleteFile($"{filePath}.meta");
+            yield return null;
+        }
+    }
+}
