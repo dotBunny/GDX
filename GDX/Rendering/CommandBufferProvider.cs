e2211bfa7f5704e4277dd3ac23f5990bb89d9c56
Matthew Davey <matthew.davey@dotbunny.com>
2022-07-29 18:24:02 -0400

    Add example usage

--- GDX/Rendering/CommandBufferProvider.cs
+++ GDX/Rendering/CommandBufferProvider.cs
@@ -19,6 +19,15 @@ namespace GDX.Rendering
         /// <summary>
         /// Get an instance of <see cref="DrawCommandBuffer"/> based on the provided <paramref name="key"/>.
         /// </summary>
+        /// <example>
+        ///     DrawCommandBuffer buffer = CommandBufferProvider.GetDrawCommandBuffer(gameObjet.GetInstanceID());
+        ///     if (!buffer.Finalized)
+        ///     {
+        ///         /// Draw lots of stuff ...
+        ///         buffer.DrawWireCube(Color.white, worldPosition, size);
+        ///     }
+        ///     buffer.Execute();
+        /// </example>
         /// <param name="key">
         ///     A value based key used to reference a <see cref="DrawCommandBuffer"/> in a
         ///     <see cref="IntKeyDictionary{TValue}"/>.
