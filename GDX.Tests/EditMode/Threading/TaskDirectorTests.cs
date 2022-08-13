fb4d1290ec798371e6f5408f99654cbd315024d0
Matthew Davey <matthew.davey@dotbunny.com>
2022-08-13 14:55:46 -0400

    Director framing

--- GDX.Tests/EditMode/Threading/TaskDirectorTests.cs
+++ GDX.Tests/EditMode/Threading/TaskDirectorTests.cs
@@ -2,6 +2,8 @@
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace GDX.Threading
@@ -11,21 +13,57 @@ namespace GDX.Threading
     /// </summary>
     public class TaskDirectorTests
     {
-        // [Test]
-        // [Category(Core.TestCategory)]
-        // public void TaskDirector_Logging()
-        // {
-        //     new TestTask(TestLiterals.HelloWorld, 5, null, null).Enqueue();
-        //
-        //     TaskDirector.Complete();
-        //
-        //     Assert.IsTrue(m_Log.Count == 5, $"{m_Log.Count} != 5");
-        // }
+        List<string> m_Log = new List<string>();
+        bool m_BlockInput = false;
+        bool m_OnCompleteCalled = false;
+        bool m_OnCompleteMainThreadCalled = false;
+
+        void LogAdded(string[] values)
+        {
+            m_Log.AddRange(values);
+        }
+
+        void BlockInput(bool inputBlocked)
+        {
+            m_BlockInput = inputBlocked;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            TaskDirector.OnLogAdded += LogAdded;
+            TaskDirector.OnBlockUserInput += BlockInput;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            TaskDirector.OnLogAdded -= LogAdded;
+            TaskDirector.OnBlockUserInput -= BlockInput;
+
+            m_BlockInput = false;
+            m_OnCompleteCalled = false;
+            m_OnCompleteMainThreadCalled = false;
+            m_Log.Clear();
+        }
+
+        [Test]
+        [Category(Core.TestCategory)]
+        public void AddLog_OnLogAdded_ExpectedCount()
+        {
+            new TaskBaseTests.TestTask(
+                TestLiterals.HelloWorld, 5, null, null).Enqueue();
+
+            TaskDirector.Wait();
+
+            Assert.IsTrue(m_Log.Count == 5, $"{m_Log.Count.ToString()} != 5");
+        }
+
+       
 
         // OnBlockUserInput
-        // OnLogAdded
 
-        // AddLog
+
         // GetBusyCount
         // GetQueueCount
         // GetStatus
