dbd0f8857f84d86beb188f729e1e58c0585f5c55
Matthew Davey <matthew.davey@dotbunny.com>
2022-08-30 10:19:17 -0400

    Capture limited support for Unsafe collections based on Collections version.

--- GDX.Tests/EditMode/Collections/Generic/UnsafeSparseSetTests.cs
+++ GDX.Tests/EditMode/Collections/Generic/UnsafeSparseSetTests.cs
@@ -1,3 +1,4 @@
+#if GDX_UNSAFE_COLLECTIONS
 // Copyright (c) 2020-2022 dotBunny Inc.
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
@@ -554,3 +555,4 @@ namespace GDX.Collections.Generic
         }
     }
 }
+#endif
--- GDX/Collections/UnsafeSparseSet.cs
+++ GDX/Collections/UnsafeSparseSet.cs
@@ -1,3 +1,4 @@
+#if GDX_UNSAFE_COLLECTIONS
 // Copyright (c) 2020-2022 dotBunny Inc.
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
@@ -1044,3 +1045,4 @@ namespace GDX.Collections
         }
     }
 }
+#endif
