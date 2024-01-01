57f848a4fc930d428a7a542764326d59ca80e753
Matthew Davey <matthew.davey@dotbunny.com>
2024-01-01 10:36:02 -0500

    Update copyright to 2024

--- GDX.RuntimeContent/ShaderContent.cs
+++ GDX.RuntimeContent/ShaderContent.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2020-2023 dotBunny Inc.
+﻿// Copyright (c) 2020-2024 dotBunny Inc.
 // dotBunny licenses this file to you under the BSL-1.0 license.
 // See the LICENSE file in the project root for more information.
 
@@ -10,7 +10,7 @@ namespace GDX.RuntimeContent
     public class ShaderContent : ScriptableObject
     {
         public static readonly int ColorPropertyID = Shader.PropertyToID("_Color");
-        
+
         public Shader DottedLine;
         public Shader UnlitColor;
     }
